724431dbf408b6362fb29df58c1b8c6b55593d5f
Pedro Palazón Candel <kusorbox@gmail.com>
2022-12-28 11:40:59 +0100

    More whitespace mess

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,7 +1,7 @@
 # install curl, git, ...
 apt-get update
 
-apt-get install -y curl git gnupg2 jq sudo zsh vim build-essential openssl
+apt-get install -y curl git gnupg2 jq sudo zsh vim build-essential openssl
 
 useradd -m user
 su user
@@ -29,17 +29,15 @@ go install github.com/go-delve/delve/cmd/dlv@latest -y
 
 # install rust
 #curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-## Install rustup and common components 
-curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y  
+## Install rustup and common components
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y
 # Check rust version:
 rustc --version
-
-# rustup install nightly 
-rustup component add rustfmt 
-# rustup component add rustfmt --toolchain nightly 
-rustup component add clippy  
-# rustup component add clippy --toolchain nightly 
-  
-cargo install cargo-expand 
-cargo install cargo-edit
+# rustup install nightly
+rustup component add rustfmt
+# rustup component add rustfmt --toolchain nightly
+rustup component add clippy
+# rustup component add clippy --toolchain nightly
+cargo install cargo-expand 
+cargo install cargo-edit
 cargo install cargo-add
