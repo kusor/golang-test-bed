7e597f0dbb1f842091c85937aa437dc1f4b2fc1b
Pedro Palazón Candel <kusorbox@gmail.com>
2022-12-28 09:45:20 +0100

    Rust toolchain

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,6 +1,16 @@
 # install curl, git, ...
 apt-get update
-apt-get install -y curl git jq
+
+apt-get install -y \ 
+   curl \ 
+   git \ 
+   gnupg2 \ 
+   jq \ 
+   sudo \ 
+   zsh \ 
+   vim \ 
+   build-essential \ 
+   openssl
 
 useradd -m user
 su user
@@ -27,6 +37,18 @@ go install github.com/go-delve/delve/cmd/dlv@latest -y
 # via: https://github.com/microsoft/vscode-go/blob/master/.travis.yml
 
 # install rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+## Install rustup and common components 
+curl https://sh.rustup.rs -sSf | sh -s -- -y  
 # Check rust version:
 rustc --version
+
+# rustup install nightly 
+rustup component add rustfmt 
+# rustup component add rustfmt --toolchain nightly 
+rustup component add clippy  
+# rustup component add clippy --toolchain nightly 
+  
+cargo install cargo-expand 
+cargo install cargo-edit
+cargo install cargo-add
